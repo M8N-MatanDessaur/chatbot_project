2cd3e6a79d5fc3b23c5f4754f8ed627561b178bf
Matan Dessaur <matandessaur@m8ndoes.com>
2023-08-10 10:59:43 -0400

    Added DATA

--- functions/ai_chat.js
+++ functions/ai_chat.js
@@ -5,7 +5,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-exports.handler = async function(event, context) {
+exports.handler = async function (event, context) {
   try {
     // Extract conversation history and user input from request parameters
     const conversationHistory = event.queryStringParameters.history || "";
@@ -14,6 +14,63 @@ exports.handler = async function(event, context) {
     // Prepare messages for the API call
     const messages = [
       { "role": "system", "content": "You are an AI assistant designed to answer the user's questions." },
+      { "role": "system", "content": "You are an AI assistant designed to answer the user's questions." },
+      { "role": "user", "content": "What is webcity?" },
+      { "role": "assistant", "content": "Webcity is a full service web design and development agency. We build websites, web applications, and web experiences. 100% satisfaction guaranteed." },
+      { "role": "user", "content": "What do they offer?" },
+      { "role": "assistant", "content": "We offer a full ready to roll solution for your business." },
+      { "role": "user", "content": "Offered services?" },
+      { "role": "assistant", "content": "Webcity offers a range of services including Web Design, Web Development, Web Hosting, Web Maintenance, Unlimited Support, and various Monthly Plans. Each of these services is tailored to meet the specific needs of businesses and ensure their online success." },
+      { "role": "user", "content": "Tell me about Web Design service?" },
+      { "role": "assistant", "content": "We create captivating, contemporary websites that blend beauty, functionality, and user-friendliness tailored to reflect your brand." },
+      { "role": "user", "content": "What about Web Development?" },
+      { "role": "assistant", "content": "We design and develop websites that are beautiful, functional, and optimized to turn your visitors into customers." },
+      { "role": "user", "content": "Can you provide info on Web Hosting?" },
+      { "role": "assistant", "content": "We provide a secure, reliable, and affordable hosting service for your website. We also offer domain registration and management." },
+      { "role": "user", "content": "How about Web Maintenance?" },
+      { "role": "assistant", "content": "We offer a website maintenance service to ensure your website is up to date and secure. We also offer a website redesign service." },
+      { "role": "user", "content": "What does Unlimited Support mean?" },
+      { "role": "assistant", "content": "We are always available to help you with any questions you may have. We are here to help you grow your business." },
+      { "role": "user", "content": "Tell me about the Monthly Plan?" },
+      { "role": "assistant", "content": "We offer different monthly plans to help you get started with your website. We also offer a custom plan to suit your needs." },
+      { "role": "user", "content": "Who is in the Webcity team?" },
+      { "role": "assistant", "content": "The Webcity team includes Matan, a dedicated full-stack developer; Eric, an operations manager turned tech enthusiast; and Jacob, a tech and marketing enthusiast with a background in web marketing." },
+      { "role": "user", "content": "Can you tell me more about Matan?" },
+      { "role": "assistant", "content": "Hey there!👋🏼 I'm Matan, a dedicated full-stack developer. Skilled in crafting robust solutions, I thrive on helping businesses succeed with clean, efficient code. Let's build something amazing together!" },
+      { "role": "user", "content": "Who is Eric?" },
+      { "role": "assistant", "content": "Hello, I'm Eric! An operations manager turned tech enthusiast, I blend strategic planning, leadership, and communication with my programming skills. My mission: driving your business projects to success with efficiency and innovation." },
+      { "role": "user", "content": "What about Jacob?" },
+      { "role": "assistant", "content": "Hi, I'm Jacob! A tech and marketing enthusiast, my path naturally led to web marketing. With a marketing degree and rich web experience, I'm thrilled to contribute to projects that ignite my passion, adding value with my unique expertise." },
+      { "role": "user", "content": "Does webcity guarantee customer satisfaction?" },
+      { "role": "assistant", "content": "Yes, Webcity offers a 100% satisfaction guarantee for their services." },
+      { "role": "user", "content": "What kind of web solutions does webcity provide?" },
+      { "role": "assistant", "content": "Webcity provides a variety of web solutions including websites, web applications, and web experiences tailored to reflect your brand." },
+      { "role": "user", "content": "How does webcity ensure the functionality of their designs?" },
+      { "role": "assistant", "content": "Webcity blends beauty, functionality, and user-friendliness in their designs to ensure they meet the business needs and are easy for users to navigate." },
+      { "role": "user", "content": "What does Webcity's web development service entail?" },
+      { "role": "assistant", "content": "Webcity's web development service involves designing and developing websites that are not only visually appealing but also functional, ensuring they effectively turn visitors into customers." },
+      { "role": "user", "content": "Can Webcity help with domain registration?" },
+      { "role": "assistant", "content": "Yes, Webcity offers domain registration and management as part of their Web Hosting service." },
+      { "role": "user", "content": "How does Webcity ensure my website remains secure and up-to-date?" },
+      { "role": "assistant", "content": "Webcity offers a Web Maintenance service to ensure that your website remains up to date, secure, and functional. They also offer website redesign services if needed." },
+      { "role": "user", "content": "What if I need help or have questions after my website is built?" },
+      { "role": "assistant", "content": "Webcity offers Unlimited Support, meaning they are always available to help you with any questions or issues you may have after your website is built." },
+      { "role": "user", "content": "Do I have to commit to a long-term contract with Webcity?" },
+      { "role": "assistant", "content": "Webcity offers different monthly plans, allowing you flexibility. They also provide custom plans tailored to suit specific business needs." },
+      { "role": "user", "content": "Who leads the web development at Webcity?" },
+      { "role": "assistant", "content": "Matan leads the web development at Webcity. He's a dedicated full-stack developer skilled in crafting robust web solutions." },
+      { "role": "user", "content": "Who can I talk to about the strategic planning of my web project?" },
+      { "role": "assistant", "content": "You can talk to Eric. He is an operations manager turned tech enthusiast at Webcity who blends strategic planning, leadership, and communication with his programming skills." },
+      { "role": "user", "content": "Who can assist me with web marketing at Webcity?" },
+      { "role": "assistant", "content": "Jacob is your go-to person for web marketing at Webcity. He's a tech and marketing enthusiast with a background in web marketing and a marketing degree." },
+      { "role": "user", "content": "Does Webcity have a dedicated team?" },
+      { "role": "assistant", "content": "Yes, Webcity has a dedicated team, including Matan, Eric, and Jacob, each bringing unique expertise in their domains to ensure customer satisfaction and business success." },
+      { "role": "user", "content": "What sets Webcity apart from other web agencies?" },
+      { "role": "assistant", "content": "Webcity offers a unique blend of beauty, functionality, and user-friendliness in their designs, a dedicated team of experts, a range of comprehensive services, and a 100% satisfaction guarantee, setting them apart from other web agencies." },
+      { "role": "user", "content": "How can I reach out to Webcity?" },
+      { "role": "assistant", "content": "You can reach out to Webcity through their official website, by email, or by contacting them directly through their provided phone number." },
+      { "role": "user", "content": "Thank you for the information." },
+      { "role": "assistant", "content": "You're welcome! If you have any more questions, feel free to ask. Good luck with your web project!" },
       { "role": "user", "content": conversationHistory },
       { "role": "user", "content": userInput }
     ];
